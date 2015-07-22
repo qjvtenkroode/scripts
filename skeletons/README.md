489958496cf85fbbeb128cbb052e7d517a57ff9f
Quincey ten Kroode <qjv.tenkroode@gmail.com>
2015-07-21 20:54:21 -0400

    more changes to the shell script and the README.md

--- skeletons/README.md
+++ skeletons/README.md
@@ -1,10 +1,14 @@
+# Pre-requisites
+
+This script assumes it is run froma project directory (for example a folder called playground, where all projects live).
+If it is used for a python project it also assumes that virtualenv is installed.
+
 # Usage
 
-> sh scripts/skeletons/skeleton.sh python <project_name> <directory>
+> sh scripts/skeletons/skeleton.sh python <project_name>
 
 The first parameter is the programming language used, for now only python.
 The second parameter is the projects name and is used to initialise:
 *  the folder names.
 *  a git repository.
 *  remote git repositories.
-The fourth parameter is the directory where the project is to be initialised.
--- skeletons/skeleton.sh
+++ skeletons/skeleton.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
-PROJECTS_DIR=$3
-
 # create directories
-mkdir $PROJECTS_DIR/$2/
-mkdir $PROJECTS_DIR/$2/$2
-mkdir $PROJECTS_DIR/$2/bin
-mkdir $PROJECTS_DIR/$2/docs
-mkdir $PROJECTS_DIR/$2/tests
+mkdir ./$2/
+mkdir ./$2/$2
+mkdir ./$2/bin
+mkdir ./$2/docs
+mkdir ./$2/tests
 
 # copy template files
-cp $PROJECTS_DIR/scripts/skeletons/$1/NAME_tests.py $PROJECTS_DIR/$2/tests/$2_tests.py
-cp $PROJECTS_DIR/scripts/skeletons/$1/setup.py $PROJECTS_DIR/$2/
-touch $PROJECTS_DIR/$2/README.md
-touch $PROJECTS_DIR/$2/$2/__init__.py
-touch $PROJECTS_DIR/$2/tests/__init__.py
+cp ./scripts/skeletons/$1/NAME_tests.py ./$2/tests/$2_tests.py
+cp ./scripts/skeletons/$1/setup.py ./$2/
+touch ./$2/README.md
+touch ./$2/$2/__init__.py
+touch ./$2/tests/__init__.py
 
 # rename template files
-sed -i.bak "s/NAME/$2/" $PROJECTS_DIR/$2/setup.py
-rm $PROJECTS_DIR/$2/setup.py.bak
-sed -i.bak "s/NAME/$2/" $PROJECTS_DIR/$2/tests/$2_tests.py
-rm $PROJECTS_DIR/$2/tests/$2_tests.py.bak
+sed -i.bak "s/NAME/$2/" ./$2/setup.py
+rm ./$2/setup.py.bak
+sed -i.bak "s/NAME/$2/" ./$2/tests/$2_tests.py
+rm ./$2/tests/$2_tests.py.bak
 
 # setup git
-git init $PROJECTS_DIR/$2/
-cp $PROJECTS_DIR/scripts/skeletons/$1/gitignore $PROJECTS_DIR/$2/.gitignore
-cat $PROJECTS_DIR/scripts/skeletons/$1/config >> $PROJECTS_DIR/$2/.git/config
-sed -i.bak "s/NAME/$2/" $PROJECTS_DIR/$2/.git/config
-rm $PROJECTS_DIR/$2/.git/config.bak
+git init ./$2/
+cp ./scripts/skeletons/$1/gitignore ./$2/.gitignore
+cat ./scripts/skeletons/$1/config >> ./$2/.git/config
+sed -i.bak "s/NAME/$2/" ./$2/.git/config
+rm ./$2/.git/config.bak
+
+# setup a virtualenv
+virtualenv ./virtualenv/$2
+source ./virtualenv/$2/bin/activate
