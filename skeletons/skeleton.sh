076581ad45878accef420649424fbb3bce3fa865
Quincey ten Kroode <qjv.tenkroode@gmail.com>
2016-09-13 08:15:00 +0200

    changes plx

--- skeletons/skeleton.sh
+++ skeletons/skeleton.sh
@@ -11,14 +11,18 @@ if [ $1 = 'python' ]; then
     # copy template files
     cp ./scripts/skeletons/$1/NAME_test.py ./$2/tests/$2_test.py
     cp ./scripts/skeletons/$1/setup.py ./$2/
+    cp ./scripts/skeletons/$1/Makefile ./$2/
+    cp ./scripts/skeletons/$1/dev_requirements.txt ./$2/
     touch ./$2/$2/__init__.py
     # rename template files
     sed -i.bak "s/NAME/$2/" ./$2/setup.py
     rm ./$2/setup.py.bak
+    sed -i.bak "s/NAME/$2/" ./$2/Makefile
+    rm ./$2/Makefile.bak
     sed -i.bak "s/NAME/$2/" ./$2/tests/$2_test.py
     rm ./$2/tests/$2_test.py.bak
     # setup a virtualenv
-    virtualenv ./virtualenvs/$2
+    cd ./$2 && make bootstrap-dev && cd ../
 else
     echo 'Unknown, skipping tests files'
 fi
@@ -33,7 +37,7 @@ cat ./scripts/skeletons/$1/config >> ./$2/.git/config
 sed -i.bak "s/NAME/$2/" ./$2/.git/config
 rm ./$2/.git/config.bak
 
-# setup travis & coveralls
+# setup travis
 cp ./scripts/skeletons/$1/travis.yml ./$2/.travis.yml
 sed -i.bak "s/NAME/$2/" ./$2/.travis.yml
 rm ./$2/.travis.yml.bak
